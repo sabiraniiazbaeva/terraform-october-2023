9f2cbe71d81d404ec800b14ab8ba60040515668a
sabiraniiazbaeva <153569549+sabiraniiazbaeva@users.noreply.github.com>
2024-03-27 14:28:53 -0500

    Update key.tf

--- Class2/key.tf
+++ Class2/key.tf
@@ -1,7 +1,7 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "hello"
-  public_key = file("~/.ssh/id_rsa.pub")
+#resource "aws_key_pair" "deployer" {
+#  key_name   = "hello"
+#  public_key = file("~/.ssh/id_rsa.pub")
 
-  tags = local.common_tags
+#  tags = local.common_tags
 
-}
+#}
